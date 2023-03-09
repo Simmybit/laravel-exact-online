3802e9477c6a48aecb9b30ae744b474a4dfa372f
Simon <simon.hiemstra@mybit.nl>
2023-03-09 18:05:16 +0100

    commit

---
+++ src/Models/ExactApplication.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Simmybit\LaravelExactOnline\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ExactApplication extends Model
+{
+    protected $hidden = [
+        'client_id',
+        'client_secret',
+        'webhook_secret',
+        'access_token',
+        'refresh_token',
+        'token_expires',
+        'authorisation_code'
+    ];
+}
